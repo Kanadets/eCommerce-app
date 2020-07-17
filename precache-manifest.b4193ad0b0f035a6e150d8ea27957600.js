672baa0ba0ae86ce97f88a15892065fea543380d
Dmitrii <31366100+Kanadets@users.noreply.github.com>
2020-07-17 00:21:12 -0700

    Updates

---
+++ precache-manifest.b4193ad0b0f035a6e150d8ea27957600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630bdc9f2112062f1711ea275ac30dac",
+    "url": "/eCommerce-app/index.html"
+  },
+  {
+    "revision": "55bed4d8078e77b475a2",
+    "url": "/eCommerce-app/static/css/main.8f0d5b4d.chunk.css"
+  },
+  {
+    "revision": "42d5c24bd3c2ad87fc73",
+    "url": "/eCommerce-app/static/js/2.2412ec01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eCommerce-app/static/js/2.2412ec01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55bed4d8078e77b475a2",
+    "url": "/eCommerce-app/static/js/main.0829542e.chunk.js"
+  },
+  {
+    "revision": "b52430560c7e0486f0af",
+    "url": "/eCommerce-app/static/js/runtime-main.d48347ed.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/eCommerce-app/static/media/crown.ed28accb.svg"
+  }
+]);
