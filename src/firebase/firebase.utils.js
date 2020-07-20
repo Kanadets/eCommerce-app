5fb88d31021a78c63194081c33f847e28651afb9
Dmitrii <31366100+Kanadets@users.noreply.github.com>
2020-07-19 21:54:44 -0700

    integrated firebase into app

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyDB6wHY523PeZJ5utltjM3kvFaY9-ZYTbs",
+  authDomain: "ecmr-db.firebaseapp.com",
+  databaseURL: "https://ecmr-db.firebaseio.com",
+  projectId: "ecmr-db",
+  storageBucket: "ecmr-db.appspot.com",
+  messagingSenderId: "425269483195",
+  appId: "1:425269483195:web:d2d590d4a6a9beeb677b9e",
+  measurementId: "G-CN463WSHN3",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
