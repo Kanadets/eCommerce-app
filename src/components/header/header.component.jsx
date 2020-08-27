b1c3d96d8dd568e386be153cda7efde3d16613cd
Dmitrii <fomindmitrii@iclud.com>
2020-08-26 19:22:00 -0700

    started working on shopping cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component"
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -27,6 +28,7 @@ const Header = ({ currentUser }) => {
         ) : (
             <Link to="/signin">SIGN IN</Link>
           )}
+        <CartIcon />
       </div>
     </div>
   );
