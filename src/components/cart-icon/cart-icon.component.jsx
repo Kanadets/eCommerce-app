0606ebb08ea871e3d9c591bead1112e6b1d1b324
Dmitrii <31366100+Kanadets@users.noreply.github.com>
2020-08-31 17:08:34 -0700

    added dropdown cart component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-import './cart-icon.styles.scss';
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-const CartIcon = () => {
-    return (
-        <div className="cart-icon">
-            <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
-        </div>
-    )
-}
+import "./cart-icon.styles.scss";
 
-export default CartIcon;
+const CartIcon = ({ toggleCartHidden }) => {
+  return (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">0</span>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
