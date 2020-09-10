50286cf6f2aaddfd876cf538cab2fcf492e3a288
Dmitrii <31366100+Kanadets@users.noreply.github.com>
2020-09-10 01:28:02 -0700

    added clearItemFromCart action and bound it to remove in checkout-item & added addItem and removeItem functionality to checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -11,7 +11,7 @@ import {
 
 import "./checkout.styles.scss";
 
-const CheckoutPage = ({ cartItems, total }) => (
+const CheckoutPage = ({ cartItems, total, dispatch }) => (
   <div className="checkout-page">
     <div className="checkout-header">
       <div className="header-block">
