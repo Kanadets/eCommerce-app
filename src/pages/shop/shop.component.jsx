5b75fc7d17fb1e570cf9cb4a074eadf0601458b9
Dmitrii <fomindmitrii@iclud.com>
2020-09-11 19:00:53 -0700

    moved all states into redux and made local storage to keep cart data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,22 @@
-import React, { Component } from "react";
-import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+import React from 'react'
+import { createStructuredSelector } from 'reselect'
+import { connect } from 'react-redux'
 
-import SHOP_DATA from "./shop.data.js";
+import { selectCollections } from '../../redux/shop/shop.selectors'
+import CollectionPreview from '../../components/preview-collection/collection-preview.component'
 
-class ShopPage extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  )
 }
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+})
+
+export default connect(mapStateToProps)(ShopPage)
